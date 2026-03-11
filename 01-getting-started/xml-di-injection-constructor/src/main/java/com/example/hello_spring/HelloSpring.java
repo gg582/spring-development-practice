ccef398948a3f267fef9551bf3d8c564f247d3ed
이윤진(Lee Yunjin) <168180007+gg582@users.noreply.github.com>
2026-03-11 15:42:25 +0900

    Refactor comments in HelloSpring class
    Remove duplicate comment about the Musician member variable.

--- 01-getting-started/xml-di-injection-constructor/src/main/java/com/example/hello_spring/HelloSpring.java
+++ 01-getting-started/xml-di-injection-constructor/src/main/java/com/example/hello_spring/HelloSpring.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class HelloSpring {
+	// 이것은 최소한의 받은 인자들에 대한 인삿말 조합을 하기 위해 클래스를 현재 HelloSpring 컨트롤러의 멤버로 대입합니다.
 	private final Musician musician;
-// 이것은 최소한의 받은 인자들에 대한 인삿말 조합을 하기 위해 클래스를 현재 HelloSpring 컨트롤러의 멤버로 대입합니다.
 	// 현재 이것을 사용하는 클래스는 BluesMusician입니다.
 	// 추후 XML DI 실습을 위해서 추가 클래스를 선언할 것입니다.
 	// DATE: 2026-03-10 15:35 UTC+0900 작성
